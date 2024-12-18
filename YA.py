c95be42b31e11e72956f6c0a81718ec4e475eee4
Youssef4800 <youssea70@gmail.com>
2024-12-18 04:05:38 +0200

    I added YA file to test some functions in it

---
+++ YA.py
@@ -0,0 +1,82 @@
+import numpy as np
+import pygame
+import sys
+import math
+from threading import Timer
+import random
+
+
+ROWS = 6
+COLS = 7
+
+PLAYER_TURN = 0
+AI_TURN = 1
+
+PLAYER_PIECE = 1
+AI_PIECE = 2
+
+# colors for GUI
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+SQUARESIZE = 100
+
+
+def create_board():
+    board = np.zeros((ROWS, COLS))
+    return board
+
+
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+
+def get_next_open_row(board, col):
+    for r in range(ROWS-1, -1, -1): # 5 4 3 2 1 0  move from bottom to top to find which row is empty
+        if board[r][col] == 0:
+            return r
+        
+
+def winning_move(board, piece):
+    # checking horizontal 'windows' of 4 for win
+    for c in range(COLS-3):
+        for r in range(ROWS):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+
+    # checking vertical 'windows' of 4 for win
+    for c in range(COLS):
+        for r in range(ROWS-3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+
+    # checking positively sloped diagonals for win
+    for c in range(COLS-3):
+        for r in range(3, ROWS):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
+            
+    # checking negatively sloped diagonals for win
+    for c in range(3,COLS):
+        for r in range(3, ROWS):
+            if board[r][c] == piece and board[r-1][c-1] == piece and board[r-2][c-2] == piece and board[r-3][c-3] == piece:
+                return True
+
+
+def draw_board(board):
+    for c in range(COLS):
+        for r in range(ROWS):
+            pygame.draw.rect(screen, BLUE, (c * SQUARESIZE, r * SQUARESIZE + SQUARESIZE, SQUARESIZE, SQUARESIZE ))
+            if board[r][c] == 0:
+                pygame.draw.circle(screen, BLACK, (int(c * SQUARESIZE + SQUARESIZE/2), int(r* SQUARESIZE + SQUARESIZE + SQUARESIZE/2)), circle_radius)
+            elif board[r][c] == 1:
+                pygame.draw.circle(screen, RED, (int(c * SQUARESIZE + SQUARESIZE/2), int(r* SQUARESIZE + SQUARESIZE + SQUARESIZE/2)), circle_radius)
+            else :
+                pygame.draw.circle(screen, YELLOW, (int(c * SQUARESIZE + SQUARESIZE/2), int(r* SQUARESIZE + SQUARESIZE + SQUARESIZE/2)), circle_radius)
+
+    pygame.display.update()
+
+
+################### ignore any errors for now 
